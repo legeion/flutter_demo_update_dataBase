5c5eefacba2e955e4a4e09d834ad5e69ce4452ec
Yannick ZEVOUNOU <yannopod@gmail.com>
2022-04-27 23:10:56 +0200

    refactor: Mise à jour des sources pour plus de clarté

--- lib/db/HistoryDb.dart
+++ lib/db/HistoryDb.dart
@@ -1,97 +1,55 @@
+/// dbHistory
+/// Classe d'historisation du cycle de vie de la BDD `school.db`.
+///
+/// Cette classe va contenir les instructions Sql de création (init)
+/// de la BDD ainsi que les instructions de mise à jour au fil des
+/// versions
+///
+/// La notion de versionning des modification est portée par la
+/// constante de classe `migrationScripts`.
 class HistoryDB {
-  /* ///Courses table V1
-  /// Exploité dans la version db:
-  Future<void> _tableCoursesV1(Database db) async {
-    await db.execute(
-        'create table courses(id integer primary key autoincrement, name varchar(50), content varchar(255), hours integer)');
-  }
+  static final String dbName = "school.db";
 
-  /// Update Courses table V1 to V2
-  /// Exploité dans la version db:
-  Future<void> _updateTableCoursesV1toV2(Database db) async {
-    await db.execute("alter table courses add column level varchar(50) ");
-  }
-
-  /// Create Classe table V1
-  /// Exploité dans la version db:
-  Future<void> _createTableClasseV2(Database db) async {
-    await db.execute('''create table classe(
+  static const initScripts = [
+    // Table courses
+    '''
+      create table courses(
       id integer primary key autoincrement,
-      name varchar(50),
-      average integer)''');
-  }
-
-  /// Update Courses table V2 to V3
-  /// Exploité dans la version db:
-  Future<void> _updateTableCoursesV2toV3(Database db) async {
-    await db.execute("alter table courses add column level integer ");
-  } */
-
-  /**
-   * Les constructeurs de la base de donnée en fonction de l'évolution de l'application
-   */
-
-  /// YZE
-  static const initScripts1 = [
-    // Table 1
+      name varchar(50), 
+      content varchar(255),
+      hours integer
+      )
+    ''',
+    // Table student
     '''
-          create table courses(
-            id integer primary key autoincrement,
-            name varchar(50), 
-            content varchar(255),
-            hours integer
-            )
-      ''',
-    // Table 1
+      create table student(
+        id integer primary key autoincrement,
+        name varchar(50)
+        )
     '''
-          create table student(
-            id integer primary key autoincrement,
-            name varchar(50)
-            )
-      '''
   ];
 
-  ///version db: exploité lors de la tout premiere création de l'application
-  List<String> contructorDataBaseInit() {
-    const initScript = [
-      '''
-          create table courses(
-            id integer primary key autoincrement,
-            name varchar(50), 
-            content varchar(255),
-            hours integer
-            )
-      ''',
-      '''
-          create table student(
-            id integer primary key autoincrement,
-            name varchar(50)
-            )
-      '''
-    ];
-    return initScript;
-  }
+  static const migrationScripts = [
+    // Modification x ...
+    '''
+      alter table courses add column level varchar(50)
+    ''',
 
-  ///Cette fonction est exploité lors de la mise en place du premier versionnage de la base de donnée
-  List<String> contructorDataBaseUpdate() {
-    const migrationScripts = [
-      '''
-        alter table courses add column level varchar(50)
-      ''',
-      '''
-          create table classe(
-            id integer primary key autoincrement,
-            name varchar(50),
-            average integer
-            )
-      ''',
-      '''
-          create table note(
-            id integer primary key autoincrement,
-            note integer
-            )
-      ''',
-    ];
-    return migrationScripts;
-  }
+    // Modification liée au passage en version v1.0.1
+    '''
+      create table classe(
+        id integer primary key autoincrement,
+        name varchar(50),
+        average integer
+        )
+    ''',
+
+    // Modification z ...
+    '''
+      create table note(
+        id integer primary key autoincrement,
+        note integer
+        )
+    ''',
+  ];
 }
--- lib/db/dbhelper.dart
+++ lib/db/dbhelper.dart
@@ -4,73 +4,42 @@ import 'package:sqflite/sqflite.dart';
 import '../model/course.dart';
 import 'HistoryDb.dart';
 
+/// Classe singletone permettant d'ouvrir la connexion à la BDD et
+/// de mettre à disposition une instance de la classe Database.
+///
+/// Cette classe expose par ailleurs des methodes de lecture/écriture
+/// dans la BDD
 class DataBaseApp {
+  static Database _db;
   static final DataBaseApp _instance = DataBaseApp.internal();
+
   factory DataBaseApp() => _instance;
 
   DataBaseApp.internal();
-  static Database _db;
 
   Future<Database> createDatabase() async {
     if (_db != null) {
       return _db;
     }
 
-    /// Initialisation des scripts pour la création et la mise à jour des tables
-    List<String> migrationScripts = HistoryDB().contructorDataBaseUpdate();
-    List<String> initScript = HistoryDB().contructorDataBaseInit();
+    // Definition du path (chemin) de la BDD
+    String path = join(await getDatabasesPath(), HistoryDB.dbName);
 
-    /// Lancement des procés de mise en place de la base de donnée
-    String path = join(await getDatabasesPath(), 'school.db');
-    _db = await openDatabase(path, version: migrationScripts.length,
+    // Création (Ouverture) de l'instance BDD
+    _db = await openDatabase(path, version: HistoryDB.migrationScripts.length,
         onCreate: (Database db, int version) async {
-      //initScript.forEach((script) async => await db.execute(script));
-      HistoryDB.initScripts1
-          .forEach((script) async => await db.execute(script));
-      /* if () {
-        for (var i = oldVersion - 1; i <= migrationScripts.length - 1; i++) {
-        await db.execute(migrationScripts[i]);
-      }
-      } */
-      print("TODO RODO: base de donnée créer");
+      HistoryDB.initScripts.forEach((script) async => await db.execute(script));
+      print(
+          "[DEBUG-TEST]: Creation/Initialisation de la BDD <${HistoryDB.dbName}> !");
     }, onUpgrade: (Database db, int oldVersion, int newVersion) async {
-      //await deleteDatabase(path);
-      print("TODO RODO: new version: $newVersion");
-      print("TODO RODO: ancien version: $oldVersion");
+      print(
+          "[DEBUG-TEST]: Mise à jour de la BDD de la v$oldVersion vers la v$newVersion !");
       for (var i = oldVersion - 1; i <= newVersion - 1; i++) {
-        await db.execute(migrationScripts[i]);
+        await db.execute(HistoryDB.migrationScripts[i]);
       }
-      print("TODO RODO: base de donnée mise à jour");
     });
     return _db;
   }
-
-  /*  Future<Database> createDatabase() async {
-    if (_db != null) {
-      return _db;
-    }
-    //define the path to the database
-    String path = join(await getDatabasesPath(), 'school.db');
-    _db = await openDatabase(path, version: 2,
-        onCreate: (Database db, int v) async {
-      //create tables
-      await HistoryDB().contructorDataBaseV1(db);
-      print("TODORO: creation de la base de donnée");
-    }, onUpgrade: (Database db, int oldV, int newV) async {
-      print('Version en cour:$oldV');
-      print('Nouvelle version:$newV');
-      if (oldV < newV && oldV + 1 == newV) {
-        /// Migration normal: la version de l'application trouver sur le device à un numéro (n-1) de l'application qui veut être installer
-        print('Migration normal');
-        await HistoryDB().contructorDataBaseV3(db);
-      } else {
-        /// Migration progressive: la version de l'application trouver sur le device à un numéro (n-n') de l'application qui veut être installer
-        print('Migration progressive:${oldV + 1}');
-        await HistoryDB().constructorMigrationProgressive(oldV + 1, db);
-      }
-    });
-    return _db;
-  } */
 
   Future<int> createCourse(Course course) async {
     Database db = await createDatabase();
